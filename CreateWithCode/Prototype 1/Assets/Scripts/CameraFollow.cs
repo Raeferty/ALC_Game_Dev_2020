7b77fd88db82fd1d220a1ef149cf25eb47b2c8cc
Raef Grierson <raef.g227@stu.nebo.edu>
2019-09-18 10:29:33 -0600

    Added vehicle movement

--- CreateWithCode/Prototype 1/Assets/Scripts/CameraFollow.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 offset = new Vector3(0, 8, -14);
 
     // Update is called once per frame
     void Update()
