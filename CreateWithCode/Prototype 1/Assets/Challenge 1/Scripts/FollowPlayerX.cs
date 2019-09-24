ee9f352e20dcbe78e62b7d6e2c6e7eaa734da53b
Raef Grierson <raef.g227@stu.nebo.edu>
2019-09-24 10:30:13 -0600

    Completed plane project and propeller

--- CreateWithCode/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ CreateWithCode/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3(50, 0, 0);
+    private Vector3 offset = new Vector3(50, 0, 5);
 
     // Update is called once per frame
     void Update()
