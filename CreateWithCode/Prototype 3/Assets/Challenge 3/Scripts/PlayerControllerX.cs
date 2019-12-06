f650ba56e05bf180f970d614b7679ab4d9d3d7a1
Raef Grierson <raef.g227@stu.nebo.edu>
2019-12-06 10:30:41 -0700

    Worked on challenge 3

--- CreateWithCode/Prototype 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ CreateWithCode/Prototype 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public bool gameOver;
+    public bool isLowEnough;
 
     public float floatForce;
     private float gravityModifier = 1.5f;
@@ -34,7 +35,7 @@ public class PlayerControllerX : MonoBehaviour
     void Update()
     {
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKey(KeyCode.Space) && !gameOver && isLowEnough)
         {
             playerRb.AddForce(Vector3.up * floatForce);
         }
